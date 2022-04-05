2e223ea0fa07242e9d6c4e3aa60e5069d9c6f980
WillianPrates <willian.ps97@gmail.com>
2022-04-05 14:59:14 -0300

    login page ok, ajustar cor botao gradiente

---
+++ app/src/main/java/com/example/conectepet/FormLogin.java
@@ -0,0 +1,15 @@
+package com.example.conectepet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+        getSupportActionBar().hide();
+    }
+}
